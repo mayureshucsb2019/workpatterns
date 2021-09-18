c87a788c00de82cea3541f5b32d1b6e935e054f3
mayuresh.anand <mayuresh.anand@accenture.com>
2021-09-17 18:08:42 -0700

    new changes

--- utils/utils.js
+++ utils/utils.js
@@ -4,11 +4,17 @@ const EmailResponseTimeSchema = require("../models/emailResponseTimeSchema");
 const fs = require("fs");
 const crypto = require("crypto");
 
-const emailTimeStampMap = {};
-const emailTimeStampArrayObj = [];
+const domainTimeStampMap = {};
 
 const sleep = function (msec) {
     return new Promise(resolve => setTimeout(resolve, msec * 1000));
+}
+
+const sortByAbsoluteValue = function (numbers) {
+    numbers.sort(function (a, b) {
+        return Math.abs(a) - Math.abs(b);
+    })
+    return numbers
 }
 
 const storeJsonToDb = function () {
@@ -24,14 +30,14 @@ const storeJsonToDb = function () {
     });
 }
 
-const buildKeyValueStore = async function () {
+const buildKeyValueStore = async function (userEmail) {
+    // console.log("USER: ", userEmail);
     let fileContent = JSON.parse(fs.readFileSync("./emails_aug_4.json"));
     let receivers = [];
     let sender = "";
     let time = 0;
-    let hash_of_string = "";
-    let senders_map = {};
     for (let i = 0; i < fileContent.length; i++) {
+        // console.log(fileContent[i])
         // get the list of addresses
         addresses = fileContent[i].addresses;
         // there is only one sender, that is what I am assuming here unless changed
@@ -40,47 +46,74 @@ const buildKeyValueStore = async function () {
         receivers = [];
         // get the time stamp
         time = fileContent[i].time;
-        // Extract the receivers and the senders
+        // Extract the receivers and the sender
         addresses.forEach(person => {
             if (person.role == "sender") {
                 sender = person.address;
-                if(!senders_map.hasOwnProperty(sender)){
-                    senders_map[sender] = 1
-                }else{
-                    senders_map[sender] +=1
-                }
             } else {
                 receivers.push(person.address);
             }
         });
-        // make hash(sender_receiver) map for all times
-        receivers.forEach(receiver => {
-            string_to_hash = sender + receiver
-            console.log("-->", string_to_hash)
-            // hash the string
-            const md5Hasher = crypto.createHmac("md5", md5Secret);
-            hash_of_string = md5Hasher.update(string_to_hash).digest("hex");
-            // console.log("Hash and string:", hash_of_string)
-            if (emailTimeStampMap.hasOwnProperty(hash_of_string)) {
-                // console.log("Map value for ", hash_of_string, emailTimeStampMap[hash_of_string])
-                emailTimeStampMap[hash_of_string].push(time)
-            } else {
-                emailTimeStampMap[hash_of_string] = [time];
+        // console.log(`SENDER: ${sender} :: RECEIVERS: ${receivers}`);
+        // if sender is the user in question then push the time stamp value 
+        // store the timestamp value only once for the organisation map.. use set
+        if (sender == userEmail) {
+            // console.log("Sender is USER.")
+            receivers.forEach(receiver => {
+                receiver = receiver.split("@")[1];
+                // if (receiver == "company_3646739100.com") {
+                //     console.log(`receiver--> ${time}`)
+                // }
+                // console.log(`${i}: USER --> ${receiver}`);
+                if (domainTimeStampMap.hasOwnProperty(receiver)) {
+                    domainTimeStampMap[receiver].add(time);
+                } else {
+                    domainTimeStampMap[receiver] = new Set([time]);
+                }
+            })
+        } else {
+            // other party is sender and user is one of the receiver, then it interacts with all org
+            let senderIndex = receivers.indexOf(userEmail);
+            if (senderIndex != -1) {
+                // console.log("Receiver is USER.")
+                // console.log(`${i}: ${sender} --> USER`);
+                sender = sender.split("@")[1];
+                // if (sender == "company_3646739100.com") {
+                //     console.log(`sender--> ${time}`)
+                // }
+                if (domainTimeStampMap.hasOwnProperty(sender)) {
+                    domainTimeStampMap[sender].add(-time);
+                } else {
+                    domainTimeStampMap[sender] = new Set([-time]);
+                }
+                receivers.forEach(receiver => {
+                    if (receiver != userEmail) {
+                        receiver = receiver.split("@")[1];
+                        // if (receiver == "company_3646739100.com") {
+                        //     console.log(`sender<-->receiver ${time}`)
+                        // }
+                        // console.log(`${i}: USER --> ${receiver}`);
+                        if (domainTimeStampMap.hasOwnProperty(receiver)) {
+                            domainTimeStampMap[receiver].add(-time);
+                        } else {
+                            domainTimeStampMap[receiver] = new Set([-time]);
+                        }
+                    }
+                })
             }
-        })
+        }
     }
-
+    console.log(Object.keys(domainTimeStampMap).length);
+    const domainTimeStampMapObj = [];
     // write the parsed data to the file to be processed by schema for multiple entry input
-    for (let [key, value] of Object.entries(emailTimeStampMap)) {
-        emailTimeStampArrayObj.push({ key: key, times: value })
+    for (let [key, value] of Object.entries(domainTimeStampMap)) {
+        domainTimeStampMapObj.push({ key: key, times: [...value] })
     }
-    fs.writeFileSync("./emailTimeStampArrayObj.json", JSON.stringify(emailTimeStampArrayObj));
-    console.log(senders_map, "\n", Object.keys(senders_map),Object.keys(senders_map).length)
+    fs.writeFileSync("./emailTimeStampArrayObj.json", JSON.stringify(domainTimeStampMapObj));
+    // console.log(senders_map, "\n", Object.keys(senders_map), Object.keys(senders_map).length)
 }
 
-const buildOrgResponseTimeMap = async function(){
-    
-}
+// buildKeyValueStore("noah@workpatterns.ai")
 
 const storeKeyValueToDb = function () {
     // insert the values into the database
@@ -95,10 +128,14 @@ const storeKeyValueToDb = function () {
 }
 
 const fetchEmailReplyTimes = function (emailId) {
-
+    const fileContent = JSON.parse(fs.readFileSync("./emailTimeStampArrayObj.json"));
+    console.log(fileContent);
 }
+
+fetchEmailReplyTimes();
 
 exports.storeKeyValueToDb = storeKeyValueToDb;
 exports.buildKeyValueStore = buildKeyValueStore;
 exports.storeJsonToDb = storeJsonToDb;
+exports.sortByAbsoluteValue = sortByAbsoluteValue;
 exports.sleep = sleep;
